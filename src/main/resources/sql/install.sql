adf44efc547f64021afdcbd6c1a284029a79435e
Administrator <aa>
2018-08-01 15:48:38 +0800

    增加用户登录功能

--- src/main/resources/sql/install.sql
+++ src/main/resources/sql/install.sql
@@ -1,7 +1,20 @@
-create table province(id int(4) primary key auto_increment,
+--省份表
+create table province(
+id int(4) primary key auto_increment,
 name varchar(32) not null,
 area double(10,3),
 populations int(10));
+
+-- 用户表
+create table user(
+id int(4) primary key auto_increment,
+name varchar(32) not null,
+password varchar(32) not null,
+age int(3),
+gender int(1),
+phonenum char(11),
+email varchar(32)
+);
 
 -- 初始化数据
 insert into province(name, area, populations) values ("江西省", 166999.361, 45120002);
