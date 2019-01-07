760142fb7e2a0bcddcb5ae4981d1bd877b2364b1
monvict <caomonvic@163.com>
2019-01-07 23:14:42 +0800

    增加工具类

---
+++ src/main/java/com/lotus/utils/DateUtils.java
@@ -0,0 +1,31 @@
+package com.lotus.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateUtils {
+
+    public static final DateTimeFormatter DEFAULT_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+
+    /***
+     * 给当前时间加上N秒
+     */
+    public static String addSeconds(String pre, long addValue) {
+        LocalDateTime localDateTime = LocalDateTime.parse(pre, DEFAULT_FORMATTER);
+        return localDateTime.plusSeconds(addValue).format(DEFAULT_FORMATTER);
+    }
+
+    /**
+     * 给当前时间增加N分钟
+     */
+    public static String addMinuts(String pre, long minuteValue) {
+        return addSeconds(pre, minuteValue * 60);
+    }
+
+    /**
+     * 给当前时间加上N小时
+     */
+    public static String addHours(String pre, long minuteValue) {
+        return addMinuts(pre, minuteValue * 60);
+    }
+}
