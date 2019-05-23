d4c571aaf671d258492ddcd811da359a7b5452f4
monvict <caomonvic@163.com>
2019-05-23 23:45:11 +0800

    精简一些配置

--- src/main/resources/sql/install.sql
+++ src/main/resources/sql/install.sql
@@ -1,28 +1,7 @@
---省份表
-create table province(
-id int(4) primary key auto_increment,
-name varchar(32) not null,
-area double(10,3),
-populations int(10));
-
--- 用户表
-create table user(
-id int(4) primary key auto_increment,
-name varchar(32) not null,
-password varchar(32) not null,
-age int(3),
-gender int(1),
-phonenum char(11),
-email varchar(32)
-);
 
 create table city(
 id int(4) primary key auto_increment,
-provinceId int(4) not null
+province_id int(4) not null,
+city_name VARCHAR(128),
+description VARCHAR(512)
 );
-
--- 初始化数据
-insert into province(name, area, populations) values ("江西省", 166999.361, 45120002);
-insert into province(name, area, populations) values ("广东省", 179736.258, 111690962);
-insert into province(name, area, populations) values ("湖南省", 211817.321, 68221247);
-insert into province(name, area, populations) values ("湖北省", 185926.657, 59023678);
